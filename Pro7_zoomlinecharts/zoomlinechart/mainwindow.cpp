370123655ba22aa61ec0fcaa1258ef81cf4de8fc
Way <zhangweisda@163.com>
2019-05-05 22:41:13 +0800

    注释不需要的代码.  TODO: 仿照vector的上下多行的形式,显示多个波形

--- Pro7_zoomlinecharts/zoomlinechart/mainwindow.cpp
+++ Pro7_zoomlinecharts/zoomlinechart/mainwindow.cpp
@@ -25,34 +25,34 @@ MainWindow::MainWindow()
 {
 //    setWindowTitle(tr("window2"));
 //![1]
-    QLineSeries *series = new QLineSeries();
-    for (int i = 0; i < 500; i++) {
-        QPointF p((qreal) i, qSin(M_PI / 50 * i) * 100);
-        p.ry() += QRandomGenerator::global()->bounded(20);
-        *series << p;
-    }
-    QLineSeries *series2 = new QLineSeries();
-    for (int i = 0; i < 500; i++) {
-        QPointF p((qreal) i, qSin(M_PI / 100 * i) * 100);
-        p.ry() += QRandomGenerator::global()->bounded(20);
-        *series2 << p;
-    }
+//    QLineSeries *series = new QLineSeries();
+//    for (int i = 0; i < 500; i++) {
+//        QPointF p((qreal) i, qSin(M_PI / 50 * i) * 100);
+//        p.ry() += QRandomGenerator::global()->bounded(20);
+//        *series << p;
+//    }
+//    QLineSeries *series2 = new QLineSeries();
+//    for (int i = 0; i < 500; i++) {
+//        QPointF p((qreal) i, qSin(M_PI / 100 * i) * 100);
+//        p.ry() += QRandomGenerator::global()->bounded(20);
+//        *series2 << p;
+//    }
 //![1]
 
-    Chart *chart = new Chart();
-    series->setName("series1");
-    series->setUseOpenGL(true);
-    chart->addSeries(series);
-    series2->setUseOpenGL(true);
-    chart->addSeries(series2);
-    chart->setTitle("Zoom in/out example");
-    chart->setAnimationOptions(QChart::NoAnimation);
-    chart->legend()->setAlignment(Qt::AlignLeft);
-    chart->legend()->show();
-    chart->createDefaultAxes();
+//    Chart *chart = new Chart();
+//    series->setName("series1");
+//    series->setUseOpenGL(true);
+//    chart->addSeries(series);
+//    series2->setUseOpenGL(true);
+//    chart->addSeries(series2);
+//    chart->setTitle("Zoom in/out example");
+//    chart->setAnimationOptions(QChart::NoAnimation);
+//    chart->legend()->setAlignment(Qt::AlignLeft);
+//    chart->legend()->show();
+//    chart->createDefaultAxes();
 
-    ChartView *chartView = new ChartView(chart);
-    chartView->setRenderHint(QPainter::Antialiasing);
+//    ChartView *chartView = new ChartView(chart);
+//    chartView->setRenderHint(QPainter::Antialiasing);
 
 //    setCentralWidget(chartView); // 此处注释掉
     std::cout << "tet";
